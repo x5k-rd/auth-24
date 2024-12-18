2c47bff44b711f2f77aefacb5c31ef5db1d56c89
x5k-rd <pavel.ivanov.ivp@outlook.com>
2024-12-18 11:58:52 +0000

    number one home route created

---
+++ app.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+// Import Express
+const express = require('express')
+
+// Create simple app - Express being used as a simple app
+const app = express()
+
+// export app to listen
+module.exports = app
+
+// Declare a dummy route to test, to send a message when tested
+// Make a get request on / which is home route
+// Has access to request and respond
+// short response warpped in h1
+app.get("/", (req, res) => {
+    res.send("<h1>Server is running</h1>")
+})
--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hello");
+// Listen to Express app
+
+// Import App
+const app = require('./app')
+
+// Grab a simple variable by extracting it, then store it
+const {PORT} = process.env
+// Use Port variable and fire a callback, also listen to the port
+app.listen(PORT, () =>{
+    console.log ('Server is running at por: ${PORT}');
+})
+
+
