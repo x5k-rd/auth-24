1b2b0d8bd6645884ca77b711800bef127461ff49
x5k-rd <pavel.ivanov.ivp@outlook.com>
2024-12-18 14:46:07 +0000

    DB update

--- DB/database.js
+++ DB/database.js
@@ -7,11 +7,10 @@ const { MONGODB_URL } = process.env
 // Export to be used by other files
 exports.connect = () => {
     console.log(typeof MONGODB_URL);
-    mongoose.connect(MONGODB_URL, {
-        userNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then()
+    mongoose.connect(MONGODB_URL)
+    .then(
+        console.log('DB connected successfully')
+    )
     .catch((error) => {
         console.log('DB connection failed');
         console.log(error);
